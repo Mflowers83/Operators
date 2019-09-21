2e4392fd568a1e2bcd18dd66089834c002d8f129
Mike <55030551+Mflowers83@users.noreply.github.com>
2019-09-21 14:19:42 -0400

    Add files via upload
    OperatorsProject
    
    Coverting feet & inches to centimeters

---
+++ Operatorsproject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operatorsproject;
+
+/**
+ *
+ * @author Michael.Flowers
+ */
+public class Operatorsproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
